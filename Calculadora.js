9a3d7712de84a49d6f37befd492768f1eb715dc3
Cinthia M. Orona <devrider2021@gmail.com>
2022-11-29 17:08:03 -0300

    Eliminé funciones que no se utilizaban

--- Calculadora.js
+++ Calculadora.js
@@ -18,37 +18,5 @@ class Calculadora {
         return eval(num1 * num2);
     }
 
-    //Pi
-
-    pi(num1){
-        return Math.pi(num1);
-    }
-
-
-    //Tangente
-
-    tg(num1){
-        return Math.tan(num1);
-    }
-
-    //Seno de angulos
-
-    sin(num1){
-        return Math.sin(num1);
-    }
-
-    cos(num1){
-        return Math.cos(num1);
-    }
-    
-    //Logaritmos
-
-    ln(num1, num2){
-        return Math.log(num1) / Math.log(num2);
-    }
-
-    log2(num1){
-        return Math.log2(num1);
-    }
 
 } 
